78f400d9d19155f9ca4deb9534e1529c751ae730
Ofin26 <gdd3126@gmail.com>
2024-12-06 17:49:30 +0400

    Изменение в расписании

---
+++ Классы/Классы/Сhangeofchedule.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DefaultUserNamespace;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Сhangeofchedule
+{
+    internal class СhangeofChedule
+    {
+        public int NumberofLessons { get; set; }
+        public string Lessontime { get; set; }
+        public string ClassRoom { get; set; }
+        public string Teacher { get; set; }
+    }
+}
