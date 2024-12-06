6aaf93526ca3c56ac0a663a45dd518ec813cc482
Ofin26 <gdd3126@gmail.com>
2024-12-06 17:25:49 +0400

    Класс Учителей

---
+++ Классы/Классы/DefaultUser.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DefaultUserNamespace
+{
+    internal class DefaultUser
+    {
+        public string FIO { get; set; }
+        public string Stats { get; set; }
+    }
+}
---
+++ Классы/Классы/TeacherClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using DefaultUserNamespace;
+
+namespace qq
+{
+    internal class TeacherClass:DefaultUser
+    {
+        public string number { get; set; }
+        public string email { get; set; }
+    }
+}
